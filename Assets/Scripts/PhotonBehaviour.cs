c74cb20bef79ed34184cb1b50e60175dd567a11f
Engin Deniz Cengiz <engin@gamaprime.com>
2015-01-24 01:53:36 +0200

    space level physics working scene added, some sprites added

---
+++ Assets/Scripts/PhotonBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonBehaviour : MonoBehaviour {
+        
+    public Vector2 velocity = new Vector2(0, 0);
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(velocity.x, velocity.y, 0) * Time.deltaTime;
+	}
+
+    public void ApplyForce(Vector2 force)
+    {
+        velocity += force;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PlanetCore"))
+        {
+            Debug.Log("Photon Crushed!");
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/PlanetBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetBehaviour : MonoBehaviour {
+
+    public float mass;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Photon")) 
+        {
+            PhotonBehaviour photonScript = other.GetComponent<PhotonBehaviour>();
+            Vector3 f = (transform.position - other.transform.position);
+
+            float distance = f.magnitude;
+            f.Normalize();
+            Vector2 force = new Vector2(f.x, f.y);
+            force *= (mass / (distance * distance));
+
+            photonScript.ApplyForce(force);
+        }
+
+    }
+}
