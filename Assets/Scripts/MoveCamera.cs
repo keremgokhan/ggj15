77b02aec85a324be770d11077ec69291edcc31a5
Berna Kabadayı <e1942192@ceng.metu.edu.tr>
2015-01-24 01:25:49 +0200

    Add spacelevel

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+    private Vector3 CurrentCameraPosition;
+    private Vector3 LastMousePosition;
+
+    public bool canImove = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDrag()
+    {
+        if (canImove)
+        {
+            Vector3 delta = Input.mousePosition - LastMousePosition;
+            delta.y = 0;
+            delta.z = 0;
+            Camera.main.transform.position = CurrentCameraPosition + delta * 0.07f;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        LastMousePosition = Input.mousePosition;
+        CurrentCameraPosition = Camera.main.transform.position;
+    }
+}   
+    
