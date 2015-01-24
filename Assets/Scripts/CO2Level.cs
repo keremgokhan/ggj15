9b14ad44b3140091dfcb531b03180e2e11d65eeb
keremgokhan <mail@keremgokhan.net>
2015-01-24 17:52:11 +0200

    Add Main Menu

---
+++ Assets/Scripts/CO2Level.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CO2Level : MonoBehaviour {
+
+	public float BubbleVelocity = 0;
+	public int NumberOfCO2 = 2;
+	public int NumberOfO2 = 5;
+	public int NumberOfN = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject part1;
+	public GameObject part2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			
+			if(hit.collider != null)
+			{
+				string tag = hit.transform.tag;
+				Debug.Log ("Target Position: " + hit.transform.tag);
+				if (tag == "btn_play") 
+				{
+					part1.SetActive(false);
+					part2.SetActive(true);
+				}
+			}
+		}
+	
+	}
+}
