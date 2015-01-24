69180c930374836094a1911e8b16dc99855cce74
keremgokhan <mail@keremgokhan.net>
2015-01-24 19:51:27 +0200

    Main Menu fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -21,6 +21,38 @@ public class MainMenu : MonoBehaviour {
 			{
 				string tag = hit.transform.tag;
 				Debug.Log ("Target Position: " + hit.transform.tag);
+				
+				Vector3 temp = hit.transform.localScale;
+				temp.x *= 1.3f;
+				temp.y *= 1.3f;
+				hit.transform.localScale = temp;
+
+				/*if (tag == "btn_play") 
+				{
+					part1.SetActive(false);
+					part2.SetActive(true);
+				}
+				if (tag == "btn_lvl1") 
+				{
+					Application.LoadLevel(1);
+				}*/
+			}
+		}
+
+		if (Input.GetMouseButtonUp(0)) 
+		{
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			
+			if(hit.collider != null)
+			{
+				string tag = hit.transform.tag;
+				Debug.Log ("Target Position: " + hit.transform.tag);
+				
+				Vector3 temp = hit.transform.localScale;
+				temp.x /= 1.3f;
+				temp.y /= 1.3f;
+				hit.transform.localScale = temp;
+				
 				if (tag == "btn_play") 
 				{
 					part1.SetActive(false);
@@ -28,11 +60,6 @@ public class MainMenu : MonoBehaviour {
 				}
 				if (tag == "btn_lvl1") 
 				{
-					Vector3 temp = hit.transform.localScale;
-					temp.x *= 1.3f;
-					temp.y *= 1.3f;
-					hit.transform.localScale = temp;
-
 					Application.LoadLevel(1);
 				}
 			}
