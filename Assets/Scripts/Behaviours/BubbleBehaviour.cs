8994596681bcf308e6241794c44b2239371d11da
Engin Deniz Cengiz <engin@gamaprime.com>
2015-01-25 12:53:18 +0200

    zum

---
+++ Assets/Scripts/Behaviours/BubbleBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleBehaviour : MonoBehaviour {
+
+	public Vector3 randomPosition()
+	{
+		float x, y, z;
+		x = Random.Range(-5.8f, 5.0f);
+		y = Random.Range(7.0f, 60.0f);
+		z = Random.Range(-0.4f, 2.4f);
+		return new Vector3(x,y,z);
+	}
+	
+	public Vector3 randomVelocity()
+	{
+		float x, y, z;
+		x = 0;
+		y = Random.Range(-10.0f, -5.0f);
+		z = 0;
+		return new Vector3(x,y,z);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.y < -8)
+		{
+			transform.position = randomPosition();
+			transform.rigidbody.velocity = randomVelocity();
+		}
+
+	}
+}
---
+++ Assets/Scripts/Behaviours/PlanetBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetBehaviour : MonoBehaviour {
+
+    public float angle;
+    public float rotationSpeed;
+
+
+    private Transform body;
+
+    void Awake()
+    {
+        body = transform.GetChild(0).transform;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        body.localEulerAngles = new Vector3(0, 0, angle);
+    }
+
+    void Update()
+    {
+        body.Rotate(new Vector3(0, 1, 0), rotationSpeed * Time.deltaTime);
+
+    }
+
+
+}
