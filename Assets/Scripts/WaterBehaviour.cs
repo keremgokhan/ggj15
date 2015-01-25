1e25b937851fd246fa22dd9ab58d6044483da1df
Engin Deniz Cengiz <engin@gamaprime.com>
2015-01-25 12:08:38 +0200

    level 3 modified

--- Assets/Scripts/WaterBehaviour.cs
+++ Assets/Scripts/WaterBehaviour.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class WaterBehaviour : MonoBehaviour {
 
     private static float GRAVITY        = 9.81f;
-    private static float DELAY          = 5f;
+    private static float DELAY          = 2f;
 
     private float time                  = 0f;
 
     public float scaleReduction;
 
+    public Transform holes;
 
     void Update()
     {
@@ -31,6 +32,11 @@ public class WaterBehaviour : MonoBehaviour {
 
     }
 
+    void End()
+    {
+        Time.timeScale = 1;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Exit"))
@@ -39,7 +45,19 @@ public class WaterBehaviour : MonoBehaviour {
         }
         if (other.CompareTag("Hole"))
         {
-            Time.timeScale = 0;
+            Debug.Log(other.name);
+            if (other.name == "1")
+            {
+                transform.position = holes.GetChild(2).transform.position + new Vector3(2,0,0);
+            }
+            if (other.name == "2")
+            {
+                transform.position = holes.GetChild(1).transform.position + new Vector3(-2, 0, 0);
+            } 
+            if (other.name == "3")
+            {
+                transform.position = holes.GetChild(1).transform.position + new Vector3(-2, 0, 0);
+            }
         }
     }
 }
