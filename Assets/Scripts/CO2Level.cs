aa1efbf4ac519a85fc9b198257cd785620acc68f
keremgokhan <mail@keremgokhan.net>
2015-01-25 05:07:24 +0200

    Nice Commit

--- Assets/Scripts/CO2Level.cs
+++ Assets/Scripts/CO2Level.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class CO2Level : MonoBehaviour {
 
+	public GameObject overlay;
+
+	public Transform OxygenPrefab;
+	public Transform CarbonDioxidePrefab;
+	public Transform NitrogenPrefab;
+
 	public float BubbleVelocity = 0;
 	public int NumberOfCO2 = 2;
 	public int NumberOfO2 = 5;
@@ -15,6 +21,41 @@ public class CO2Level : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			
+			if(hit.collider != null)
+			{
+				string tag = hit.transform.tag;
+				Debug.Log ("Target Position: " + hit.transform.tag);
+				
+				Vector3 temp = hit.transform.localScale;
+				temp.x *= 1.2f;
+				temp.y *= 1.2f;
+				hit.transform.localScale = temp;
+			}
+		}
+
+		if (Input.GetMouseButtonUp(0)) 
+		{
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+			
+			if(hit.collider != null)
+			{
+				string tag = hit.transform.tag;
+				Debug.Log ("Target Position: " + hit.transform.tag);
+				
+				Vector3 temp = hit.transform.localScale;
+				temp.x /= 1.2f;
+				temp.y /= 1.2f;
+				hit.transform.localScale = temp;
+				
+				if (tag == "btn_play") 
+				{
+					overlay.SetActive(false);
+				}
+			}
+		}
 	}
 }
